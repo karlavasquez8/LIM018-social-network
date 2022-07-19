908f28aa7ae91a3fd8f1f28a9ed2c698ada7ec51
Gabriela Rojas <gabrieladelpilarrojas21@gmail.com>
2022-07-19 12:55:33 -0500

    Corrigiendo errores del usuario

--- src/view/loginEmail.js
+++ src/view/loginEmail.js
@@ -1,4 +1,5 @@
-import { logIn } from '../firebase/auth.js';
+/* eslint-disable default-case */
+import { logIn, observer } from '../firebase/auth.js';
 
 const loginEmail = {
   template: () => {
@@ -11,9 +12,16 @@ const loginEmail = {
       <span>Disfruta de la mejor comida creada para las personas amantes de codígo!</span>
       <form class="form-email">
         <input name="email" type="email" class="input-register" placeholder="E-mail">
+        <p class= "condicion login" id="error-usuario"></p>
         <input name="password" type="password" class ="input-register" placeholder="Contraseña">
+        <p class= "condicion login" id="error-contraseña"></p>
         <button type="submit" class="btn-log-email"> Iniciar sesión </button>
       </form>
+      <a href="#newPassword"> Recupera tu contraseña </a>
+      <div class="newRegister"> 
+        <span>¿No tienes una cuenta ?</span>
+        <a href="#Register">Registrate aqui</a>
+      </div>
     </div>
     <div class="modal-container">
     <div class="modal no-verified-email">
@@ -32,36 +40,38 @@ const loginEmail = {
   init: () => {
     const buttonLogIn = document.querySelector('.form-email');
     const modal = document.querySelector('.modal-container');
+    const errorUser = document.querySelector('#error-usuario');
+    const errorPassword = document.querySelector('#error-contraseña');
 
     buttonLogIn.addEventListener('submit', (event) => {
       event.preventDefault();
+      errorUser.innerHTML = '';
+      errorPassword.innerHTML = '';
       const formEmail = new FormData(event.target);
       logIn(formEmail.get('email'), formEmail.get('password'))
-      .then((user)=>{
-        if (!user.emailVerified){
-          modal.classList.add('show-modal')
-        } 
-      })
-      .catch(({code}) => {
-        let message = ''
-
-        switch(code) {
-          case 'auth/user-not-found':
-            message = 'usuario no encontrado';
-            break;
-          case 'auth/wrong-password':
-            message = 'contraseña incorrecta'
-            break;
-        }
-
-        console.log(message);
-      })
+        .then((user) => {
+          if (!user.emailVerified) {
+            modal.classList.add('show-modal');
+          } else {
+            window.location.hash = '#/home';
+          }
+        })
+        .catch(({ code }) => {
+          switch (code) {
+            case 'auth/user-not-found':
+              errorUser.innerHTML = 'El correo electronico que ingresaste no esta conectado a una cuenta';
+              break;
+            case 'auth/wrong-password':
+              errorPassword.innerHTML = 'Contraseña incorrecta';
+              break;
+          }
+        });
     });
-    const closeModal = document.querySelector('.btn-modal')
-    closeModal.addEventListener('click',() =>{
-      modal.classList.remove('show-modal')
-    })
-  }
+    const closeModal = document.querySelector('.btn-modal');
+    closeModal.addEventListener('click', () => {
+      modal.classList.remove('show-modal');
+    });
+  },
 };
 
 export default loginEmail;
